741703683120e2af39ae1af45f006d10c0decc3e
andrius-siup <andrius.siupinys@gmail.com>
2021-03-04 21:13:31 +0000

    To see what readyState prints into console.

--- main.js
+++ main.js
@@ -13,19 +13,11 @@ function setData(jsonData) {
 
 xhr.onreadystatechange = function() {
 
+    console.log(this.readyState);
+
     // The if statement is true then 'readyState is fully loaded' and 'status' successful responses
     if (this.readyState == 4 && this.status == 200) {
-        //1 document.getElementById("data").innerHTML = this.responseText;
-
-        // this shows type of string
-        //2 console.log(typeof(this.responseText));
-        
-        // this shows an object
-        //3console.log(typeof(JSON.parse(this.responseText)));
-
-        // now we got object logged to the console
-        //4 console.log(JSON.parse(this.responseText));
-
+    
         setData(JSON.parse(this.responseText));
     }
 };
