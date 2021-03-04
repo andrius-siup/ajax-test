eaae59139d5b1977028cc38ef03e4cc07d2cb5d7
andrius-siup <andrius.siupinys@gmail.com>
2021-03-04 20:51:18 +0000

    Used JSON.prase method to manipulate the data.

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 var xhr = new XMLHttpRequest();
 
+// GET is used to retrieving data from server, second argument is URL that we wont to retrieve.
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+xhr.send();
+
 xhr.onreadystatechange = function() {
+    
+    // The if statement is true then 'readyState is fully loaded' and 'status' successful responses
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        //document.getElementById("data").innerHTML = this.responseText;
+
+        // this shows type of string
+        //console.log(typeof(this.responseText));
+        
+        // this shows an object
+        //console.log(typeof(JSON.parse(this.responseText)));
+
+        // now we got object logged to the console
+        console.log(JSON.parse(this.responseText));
     }
 };
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
-xhr.send();
