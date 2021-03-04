29d55c94072ea66f974cd72a383926e5c56ebb99
andrius-siup <andrius.siupinys@gmail.com>
2021-03-04 21:06:02 +0000

    Create varaible and function, call that function, and store JSON.parse to that function.

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 var xhr = new XMLHttpRequest();
+var data;
+
 
 // GET is used to retrieving data from server, second argument is URL that we wont to retrieve.
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
+function setData(jsonData) {
+    data = jsonData;
+    console.log(data);
+}
+
 xhr.onreadystatechange = function() {
-    
+
     // The if statement is true then 'readyState is fully loaded' and 'status' successful responses
     if (this.readyState == 4 && this.status == 200) {
-        //document.getElementById("data").innerHTML = this.responseText;
+        //1 document.getElementById("data").innerHTML = this.responseText;
 
         // this shows type of string
-        //console.log(typeof(this.responseText));
+        //2 console.log(typeof(this.responseText));
         
         // this shows an object
-        //console.log(typeof(JSON.parse(this.responseText)));
+        //3console.log(typeof(JSON.parse(this.responseText)));
 
         // now we got object logged to the console
-        console.log(JSON.parse(this.responseText));
+        //4 console.log(JSON.parse(this.responseText));
+
+        setData(JSON.parse(this.responseText));
     }
 };
 
