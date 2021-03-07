883e7ec90cd1c816fe291d239d54b80539d8a658
andrius-siup <andrius.siupinys@gmail.com>
2021-03-07 20:54:42 +0000

    Not working, need fix.

--- main.js
+++ main.js
@@ -21,32 +21,48 @@ function getData(type, cb) {  // cb - for 'callback'
 function getTableHeaders(obj) {
     var tableHeaders = [];
 
-    Object.keys(obj).forEach(function(key) {
+    Object.keys(obj).forEach(function (key) {
         tableHeaders.push(`<td>${key}</td>`);
     });
-    
+
     return `<tr>${tableHeaders}</tr>`;
+}
+
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick = "writeToDocument('${prev}')">Previous</button>;
+                <button onclick = "writeToDocument('${next}')">Next</button>`;
+    } else if (next && !prev) {
+        return `<button onclick = "writeToDocument('${next}')">Next</button>`;
+    } else if (!next && prev) {
+        return `<button onclick = "writeToDocument('${prev}')">Previous</button>`
+    } 
 }
 
 function writeToDocument(type) {
 
     var tableRows = [];
     var el = document.getElementById("data");
-    el.innerHTML = ""; 
-    getData(type, function(data) {
+    el.innerHTML = "";
+    getData(type, function (data) {
+
+        if (data.next || data.previous) {
+            var pagination;
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
-        data.forEach(function(item) {
-           var dataRow = [];
+        data.forEach(function (item) {
+            var dataRow = [];
 
-           Object.keys(item).forEach(function(key) {
-               var rowData = item[key].toString();
-               var truncatedData = rowData.substring(0, 15);
-               dataRow.push(`<td>${truncatedData}</td>`);
-           });
-           tableRows.push(`<tr>${dataRow}</tr>`); 
+            Object.keys(item).forEach(function (key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`< td > ${truncatedData}</td > `);
+            });
+            tableRows.push(`< tr > ${dataRow}</tr > `);
         });
-       el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `< table > ${tableHeaders} ${tableRows}</table >${pagination} `;
     });
 }
