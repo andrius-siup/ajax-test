b9a59925c340b8b897c80c2dc5dd0371ef42a503
andrius-siup <andrius.siupinys@gmail.com>
2021-03-07 20:19:12 +0000

    Create new row of data for each record in the array.

--- main.js
+++ main.js
@@ -38,9 +38,13 @@ function writeToDocument(type) {
         var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-           
-            // el.innerHTML += "<p>" + item.name + "</p>";
+           var dataRow = [];
+
+           Object.keys(item).forEach(function(key) {
+               dataRow.push(`<td>${item[key]}</td>`);
+           });
+           tableRows.push(dataRow); 
         });
-       el.innerHTML = `<table>${tableHeaders}</table>`;
+       el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
