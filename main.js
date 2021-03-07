9ae0bdf64a1bfd83f5e563c32ed0b7a439eb5823
andrius-siup <andrius.siupinys@gmail.com>
2021-03-07 15:46:56 +0000

    Replace to el variable.

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function writeToDocument(type) {
         data = data.results;
 
         data.forEach(function(item) {
-             document.getElementById("data").innerHTML += "<p>" + item.name + "</p>";
+             el.innerHTML += "<p>" + item.name + "</p>";
         })
        
     });
