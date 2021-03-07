33c1e1f8da846cc8f449d719b3ff7e2f654dea2c
andrius-siup <andrius.siupinys@gmail.com>
2021-03-07 20:06:46 +0000

    Now display title on film button.

--- main.js
+++ main.js
@@ -18,18 +18,27 @@ function getData(type, cb) {  // cb - for 'callback'
 
 }
 
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+    
+    return `<tr>${tableHeaders}</tr>`;
+}
+
 function writeToDocument(type) {
     var el = document.getElementById("data");
     el.innerHTML = ""; 
     getData(type, function(data) {
         data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
 
         data.forEach(function(item) {
-            Object.keys(item).forEach(function(key) {
-                console.log(key);
-            })
-             el.innerHTML += "<p>" + item.name + "</p>";
+           
+            // el.innerHTML += "<p>" + item.name + "</p>";
         });
-       
+       el.innerHTML = `<table>${tableHeaders}</table>`;
     });
 }
